ed72b0c585083e2ad2db356b306e771f1412db39
ahuigo <1781999+ahuigo@users.noreply.github.com>
2023-01-06 18:26:10 +0800

    chore: my dev config

--- tools/test-examples.ts
+++ tools/test-examples.ts
@@ -2,29 +2,18 @@ import { join } from "../lib/deps.ts";
 import { initExampleConfig } from "./dev.ts";
 
 async function testExample(example: string) {
-<<<<<<< HEAD
   try {
     const examplePath = join("examples", example);
     await initExampleConfig(example);
-=======
-  initExampleConfig(example);
-  const examplePath = join("examples", example);
-  try {
->>>>>>> bff16da (feat: add unittest for examples)
     const cmd = [
       Deno.execPath(),
       "test",
       "-c",
       "deno.dev.json",
       "-A",
-<<<<<<< HEAD
-    ];
-    console.log("test ", examplePath, cmd);
-=======
       "./server.tsx",
     ];
-    console.log(examplePath, cmd);
->>>>>>> bff16da (feat: add unittest for examples)
+    console.log("test ", examplePath, cmd);
     const process = Deno.run({
       cmd,
       cwd: examplePath,
